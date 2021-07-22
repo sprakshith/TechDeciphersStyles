269edaa09a489c6d0ea93c68ef6dae08107d2cee
SP Rakshith <rakshith0908@gmail.com>
2021-07-22 21:43:02 +0530

    Converted Modules into Class

--- tdstyles/easy_plots.py
+++ tdstyles/easy_plots.py
@@ -1,76 +1,87 @@
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
-from . import style_css
+from tdstyles.style_css import StyleNotebook
+from tdstyles.exceptions import NotStyleNotebook
 
 
-def descriptive_plot(figsize=(8, 6), line_pos_x_axis=1, background_color=None):
+class EasyPlot:
+    def __init__(self, style_notebook=None):
+        if style_notebook:
+            if isinstance(style_notebook, StyleNotebook):
+                self.style_notebook = style_notebook
+            else:
+                raise NotStyleNotebook(f"Expected an object of Type: '{type(StyleNotebook())}' as a second argument. "
+                                       f"But instead got {type(style_notebook)}")
+        else:
+            self.style_notebook = StyleNotebook()
 
-    if background_color is None:
-        background_color = style_css.get_background_color()
+    def descriptive_plot(self, figsize=(8, 6), line_pos_x_axis=1, background_color=None):
 
-    fig, ax = plt.subplots(1, 1, figsize=figsize, facecolor=background_color)
+        if background_color is None:
+            background_color = self.style_notebook.get_background_color()
 
-    ax.set_facecolor(background_color)
+        fig, ax = plt.subplots(1, 1, figsize=figsize, facecolor=background_color)
 
-    line = lines.Line2D([line_pos_x_axis, line_pos_x_axis], [0, 1], transform=fig.transFigure, figure=fig,
-                        color='black', lw=0.4)
-    fig.lines.extend([line])
+        ax.set_facecolor(background_color)
 
-    return fig, ax
+        line = lines.Line2D([line_pos_x_axis, line_pos_x_axis], [0, 1], transform=fig.transFigure, figure=fig,
+                            color='black', lw=0.4)
+        fig.lines.extend([line])
 
+        return fig, ax
 
-def vertical_descriptive_plot(figsize=(6, 8), plots=3, plot_height=0.5, plot_width=0.75, background_color=None):
-    if plot_width > 1.4:
-        raise Exception('Plot width cannot be greater than 1.5')
+    def vertical_descriptive_plot(self, figsize=(6, 8), plots=3, plot_height=0.5, plot_width=0.75,
+                                  background_color=None):
+        if plot_width > 1.4:
+            raise Exception('Plot width cannot be greater than 1.5')
 
-    if background_color is None:
-        background_color = style_css.get_background_color()
+        if background_color is None:
+            background_color = self.style_notebook.get_background_color()
 
-    fig = plt.figure(figsize=figsize, facecolor=background_color)
-    axes = []
+        fig = plt.figure(figsize=figsize, facecolor=background_color)
+        axes = []
 
-    initial_axis = 0
-    for _ in range(plots):
-        axis = fig.add_axes([0, initial_axis, plot_width, plot_height])
-        axis.set_facecolor(background_color)
-        axes.append(axis)
-        initial_axis += plot_height + 0.1
+        initial_axis = 0
+        for _ in range(plots):
+            axis = fig.add_axes([0, initial_axis, plot_width, plot_height])
+            axis.set_facecolor(background_color)
+            axes.append(axis)
+            initial_axis += plot_height + 0.1
 
-    line_length = (plot_height + 0.1) * plots
-    line = lines.Line2D([plot_width + 0.1, plot_width + 0.1], [0 - 0.1, line_length], transform=fig.transFigure,
-                        figure=fig, color='black', lw=0.4)
-    fig.lines.extend([line])
+        line_length = (plot_height + 0.1) * plots
+        line = lines.Line2D([plot_width + 0.1, plot_width + 0.1], [0 - 0.1, line_length], transform=fig.transFigure,
+                            figure=fig, color='black', lw=0.4)
+        fig.lines.extend([line])
 
-    return fig, axes
+        return fig, axes
 
+    def horizontal_descriptive_plot(self, figsize=(8, 6), plots=3, plot_height=0.75, line_pos_y_axis=-0.1,
+                                    background_color=None):
+        if plots not in [2, 3, 4]:
+            raise Exception('Number of plots must be 2, 3 or 4')
 
-def horizontal_descriptive_plot(figsize=(8, 6), plots=3, plot_height=0.75, line_pos_y_axis=-0.1,
-                                background_color=None):
-    if plots not in [2, 3, 4]:
-        raise Exception('Number of plots must be 2, 3 or 4')
+        # Plot Position on X-axis
+        increment = {2: 1, 3: 0.7, 4: 0.5}
 
-    # Plot Position on X-axis
-    increment = {2: 1, 3: 0.7, 4: 0.5}
+        # Plot width based on number of plots required
+        plot_width = {2: 0.9, 3: 0.6, 4: 0.43}
 
-    # Plot width based on number of plots required
-    plot_width = {2: 0.9, 3: 0.6, 4: 0.43}
+        if background_color is None:
+            background_color = self.style_notebook.get_background_color()
 
-    if background_color is None:
-        background_color = style_css.get_background_color()
+        fig = plt.figure(figsize=figsize, facecolor=background_color)
+        axes = []
 
-    fig = plt.figure(figsize=figsize, facecolor=background_color)
-    axes = []
+        initial_axis = 0
+        for _ in range(plots):
+            axis = fig.add_axes([initial_axis, 0, plot_width[plots], plot_height])
+            axis.set_facecolor(background_color)
+            axes.append(axis)
+            initial_axis += increment[plots]
 
-    initial_axis = 0
-    for _ in range(plots):
-        axis = fig.add_axes([initial_axis, 0, plot_width[plots], plot_height])
-        axis.set_facecolor(background_color)
-        axes.append(axis)
-        initial_axis += increment[plots]
+        line_length = increment[plots] * (plots - 1) + plot_width[plots]
+        line = lines.Line2D([0 - 0.1, line_length + 0.1], [line_pos_y_axis, line_pos_y_axis], transform=fig.transFigure,
+                            figure=fig, color='black', lw=0.4)
+        fig.lines.extend([line])
 
-    line_length = increment[plots] * (plots - 1) + plot_width[plots]
-    line = lines.Line2D([0 - 0.1, line_length + 0.1], [line_pos_y_axis, line_pos_y_axis], transform=fig.transFigure,
-                        figure=fig, color='black', lw=0.4)
-    fig.lines.extend([line])
-
-    return fig, axes
+        return fig, axes
--- tdstyles/style_css.py
+++ tdstyles/style_css.py
@@ -4,132 +4,116 @@ import urllib.request as req
 from IPython.core.display import HTML
 
 
-current_theme = 'gray_area'
-
-dynamic_colors_list = None
-
-
-def load_css(theme=None, color_list=None):
+class StyleNotebook:
     """
     Four themes available right now: 'cyan_sisters', 'dark_matter', 'genus_lavandula' and 'gray_area'.
     If you want your own color pallete use 'dynamic_colors' theme and pass list of 3 Hexcodes as a second parameter.
-    [Background Color, Font & Border Color, Hover Color]
+    Example: color_lost = [Background Color, Font & Border Color, Hover Color]
     """
-    global current_theme
+    def __init__(self, theme=None, color_list=None):
+        self.theme = theme
+        self.color_list = color_list
 
-    response_main = req.urlopen(
-        'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/main.css')
+        if theme == 'dynamic_colors':
+            if (color_list is not None) and (len(color_list) == 3):
+                for color in color_list:
+                    match = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', color)
+                    if not match:
+                        raise Exception(f"'{color}' is Invalid Color Code. Please Enter Hexcodes only.")
+            else:
+                if color_list:
+                    raise Exception(
+                        f"When 'dynamic_colors' theme is selected the function excepts a color_list of length "
+                        f"3. But got {len(color_list)}.")
+                else:
+                    raise Exception(
+                        f"When 'dynamic_colors' theme is selected the function excepts a color_list of length "
+                        f"3. But got None Instead.")
 
-    if theme == 'dynamic_colors':
-        if (color_list is not None) and (len(color_list) == 3):
-            for color in color_list:
-                match = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', color)
-                if not match:
-                    raise Exception(f"'{color}' is Invalid Color Code. Please Enter Hexcodes only.")
+    def load_css(self):
+        response_main = req.urlopen(
+            'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/main.css')
 
-            current_theme = 'dynamic_colors'
-
+        if self.theme == 'dynamic_colors':
             response_theme = req.urlopen(
                 'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/dynamic_colors.css')
 
             response_theme_string = response_theme.read().decode('utf-8')
-            response_theme_string = response_theme_string.replace('[#COLOR_ONE#]', color_list[0])
-            response_theme_string = response_theme_string.replace('[#COLOR_TWO#]', color_list[1])
-            response_theme_string = response_theme_string.replace('[#COLOR_THREE#]', color_list[2])
-
-            global dynamic_colors_list
-            dynamic_colors_list = color_list
+            response_theme_string = response_theme_string.replace('[#COLOR_ONE#]', self.color_list[0])
+            response_theme_string = response_theme_string.replace('[#COLOR_TWO#]', self.color_list[1])
+            response_theme_string = response_theme_string.replace('[#COLOR_THREE#]', self.color_list[2])
 
             return HTML('<style>' + response_main.read().decode('utf-8') +
                         response_theme_string + '</style>')
+
+        elif self.theme == 'cyan_sisters':
+            response_theme = req.urlopen(
+                'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/cyan_sisters.css')
+
+            return HTML('<style>' + response_main.read().decode('utf-8') +
+                        response_theme.read().decode('utf-8') + '</style>')
+        elif self.theme == 'dark_matter':
+            response_theme = req.urlopen(
+                'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/dark_matter.css')
+
+            return HTML('<style>' + response_main.read().decode('utf-8') +
+                        response_theme.read().decode('utf-8') + '</style>')
+        elif self.theme == 'genus_lavandula':
+            response_theme = req.urlopen(
+                'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/genus_lavandula.css')
+
+            return HTML('<style>' + response_main.read().decode('utf-8') +
+                        response_theme.read().decode('utf-8') + '</style>')
         else:
-            if color_list:
-                raise Exception(f"When 'dynamic_colors' theme is selected the function excepts a color_list of length "
-                                f"3. But got {len(color_list)}.")
-            else:
-                raise Exception(f"When 'dynamic_colors' theme is selected the function excepts a color_list of length "
-                                f"3. But got None Instead.")
-    elif theme == 'cyan_sisters':
-        current_theme = 'cyan_sisters'
+            response_theme = req.urlopen(
+                'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/gray_area.css')
 
-        response_theme = req.urlopen(
-            'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/cyan_sisters.css')
+            return HTML('<style>' + response_main.read().decode('utf-8') +
+                        response_theme.read().decode('utf-8') + '</style>')
 
-        return HTML('<style>' + response_main.read().decode('utf-8') +
-                    response_theme.read().decode('utf-8') + '</style>')
-    elif theme == 'dark_matter':
-        current_theme = 'dark_matter'
+    def get_color_palette(self):
+        if self.theme == 'cyan_sisters':
+            return sns.color_palette(['#39ace7', '#0784b5'])
+        elif self.theme == 'dark_matter':
+            return sns.color_palette(['#757575', '#212121'])
+        elif self.theme == 'genus_lavandula':
+            return sns.color_palette(['#a176c6', '#8653af'])
+        elif (self.theme == 'dynamic_colors') and self.color_list and len(self.color_list) == 3:
+            return sns.color_palette([self.color_list[1], self.color_list[2]])
+        else:
+            return sns.color_palette(['#78909C', '#37474F'])
 
-        response_theme = req.urlopen(
-            'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/dark_matter.css')
+    def get_background_color(self):
+        if self.theme == 'cyan_sisters':
+            return '#cadeef'
+        elif self.theme == 'dark_matter':
+            return '#e0e0e0'
+        elif self.theme == 'genus_lavandula':
+            return '#e9d5eb'
+        elif (self.theme == 'dynamic_colors') and self.color_list and len(self.color_list) == 3:
+            return self.color_list[0]
+        else:
+            return '#CFD8DC'
 
-        return HTML('<style>' + response_main.read().decode('utf-8') +
-                    response_theme.read().decode('utf-8') + '</style>')
-    elif theme == 'genus_lavandula':
-        current_theme = 'genus_lavandula'
+    def get_font_color(self):
+        if self.theme == 'cyan_sisters':
+            return '#39ace7'
+        elif self.theme == 'dark_matter':
+            return '#757575'
+        elif self.theme == 'genus_lavandula':
+            return '#a176c6'
+        elif (self.theme == 'dynamic_colors') and self.color_list and len(self.color_list) == 3:
+            return self.color_list[1]
+        else:
+            return '#78909C'
 
-        response_theme = req.urlopen(
-            'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/genus_lavandula.css')
-
-        return HTML('<style>' + response_main.read().decode('utf-8') +
-                    response_theme.read().decode('utf-8') + '</style>')
-    else:
-        current_theme = 'gray_area'
-
-        response_theme = req.urlopen(
-            'https://raw.githubusercontent.com/sprakshith/TechDeciphersStyles/main/static/gray_area.css')
-
-        return HTML('<style>' + response_main.read().decode('utf-8') +
-                    response_theme.read().decode('utf-8') + '</style>')
-
-
-def get_color_palette():
-    if current_theme == 'cyan_sisters':
-        return sns.color_palette(['#39ace7', '#0784b5'])
-    elif current_theme == 'dark_matter':
-        return sns.color_palette(['#757575', '#212121'])
-    elif current_theme == 'genus_lavandula':
-        return sns.color_palette(['#a176c6', '#8653af'])
-    elif (current_theme == 'dynamic_colors') and dynamic_colors_list and len(dynamic_colors_list) == 3:
-        return sns.color_palette([dynamic_colors_list[1], dynamic_colors_list[2]])
-    else:
-        return sns.color_palette(['#78909C', '#37474F'])
-
-
-def get_background_color():
-    if current_theme == 'cyan_sisters':
-        return '#cadeef'
-    elif current_theme == 'dark_matter':
-        return '#e0e0e0'
-    elif current_theme == 'genus_lavandula':
-        return '#e9d5eb'
-    elif (current_theme == 'dynamic_colors') and dynamic_colors_list and len(dynamic_colors_list) == 3:
-        return dynamic_colors_list[0]
-    else:
-        return '#CFD8DC'
-
-
-def get_font_color():
-    global dynamic_colors_list
-    if current_theme == 'cyan_sisters':
-        return '#39ace7'
-    elif current_theme == 'dark_matter':
-        return '#757575'
-    elif current_theme == 'genus_lavandula':
-        return '#a176c6'
-    elif (current_theme == 'dynamic_colors') and dynamic_colors_list and len(dynamic_colors_list) == 3:
-        return dynamic_colors_list[1]
-    else:
-        return '#78909C'
-
-
-def get_font_dict(fontsize=14, fontweight='light', font_color=None):
-    if font_color is None:
-        font_color = get_font_color()
-    return {
-                'fontsize': fontsize,
-                'fontweight': fontweight,
-                'fontfamily': 'serif',
-                'horizontalalignment': 'center',
-                'color': font_color
-            }
+    def get_font_dict(self, fontsize=14, fontweight='light', font_color=None):
+        if font_color is None:
+            font_color = self.get_font_color()
+        return {
+            'fontsize': fontsize,
+            'fontweight': fontweight,
+            'fontfamily': 'serif',
+            'horizontalalignment': 'center',
+            'color': font_color
+        }
